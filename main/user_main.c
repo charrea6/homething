d8c5c0a381d0073035578051acf5f2ffc32298d8
Adam Charrett <adcharre@cisco.com>
2018-12-17 12:49:47 +0000

    Make the build of the iot thing more configurable

--- main/user_main.c
+++ main/user_main.c
@@ -22,38 +22,96 @@
 #include "motion.h"
 #include "humidityfan.h"
 
+//
+// Lights
+// 
+#if defined( CONFIG_LIGHTS_1) || defined(CONFIG_LIGHTS_2) || defined(CONFIG_LIGHTS_3)
 static Light_t light0;
+#endif
+#if defined(CONFIG_LIGHTS_2) || defined(CONFIG_LIGHTS_3)
+static Light_t light1;
+#endif
+#if defined(CONFIG_LIGHTS_3)
+static Light_t light2;
+#endif
+
+//
+// DHT22 : Temperature and humidity + fan
+//
+#if defined(CONFIG_DHT22)
 static DHT22Sensor_t thSensor;
-static Motion_t motion0;
+static char temperatureStr[6];
+static iotElement_t *temperatureElement;
+static iotElementPub_t *temperaturePub;
+
 static HumidityFan_t fan0;
+#endif
 
-static void temperature(void *userData, int16_t tenthsUnit)
+#if defined(CONFIG_MOTION)
+static Motion_t motion0;
+#endif
+
+#if defined(CONFIG_DHT22)
+static void temperatureUpdate(void *userData, int16_t tenthsUnit)
 {
-    printf("Temperature: %d.%d\n", tenthsUnit / 10, tenthsUnit % 10);
+    iotValue_t value;
+    sprintf(temperatureStr, "%d.%d", tenthsUnit / 10, tenthsUnit % 10);
+    value.s = temperatureStr;
+    iotElementPubUpdate(temperaturePub, value);
 }
+#endif
 
+#if defined( CONFIG_LIGHTS_1) || defined(CONFIG_LIGHTS_2) || defined(CONFIG_LIGHTS_3)
 static void lightSwitchCallback(void *userData, int state)
 {
     Light_t *light = userData;
     lightToggle(light);
 }
 
+static void setupLight(Light_t *light, int switchPin, int relayPin)
+{
+    lightInit(light, relayPin);
+    switchAdd(switchPin, lightSwitchCallback, light);
+}
+#endif
+
 void app_main(void)
 {
     ESP_ERROR_CHECK( nvs_flash_init() );
     
     iotInit(CONFIG_ROOM);
+#if defined( CONFIG_LIGHTS_1) || defined(CONFIG_LIGHTS_2) || defined(CONFIG_LIGHTS_3)
+    setupLight(&light0, 14, 5);
+#endif
+#if defined(CONFIG_LIGHTS_2) || defined(CONFIG_LIGHTS_3)
+    setupLight(&light1, 12, 4);
+#endif
+#if defined(CONFIG_LIGHTS_3)
+    setupLight(&light2, 13, 0);
+#endif
 
-    lightInit(&light0, 14);
-    dht22Init(&thSensor, 5);
-    motionInit(&motion0, 13);
-    humidityFanInit(&fan0, 14, 75);
+#if defined(CONFIG_MOTION)    
+    motionInit(&motion0, 16);
+#endif
 
-    dht22AddTemperatureCallback(&thSensor, temperature, NULL);
-    dht22AddHumidityCallback(&thSensor, (DHT22CallBack_t)humidityFanUpdateHumidity, &fan0);
+#if defined(CONFIG_DHT22) 
+    iotValue_t value;
     
-    switchAdd(4, lightSwitchCallback, &light0);
-    switchStart();
-    iotStart();
+    dht22Init(&thSensor, 4);
+    sprintf(temperatureStr, "0.0");
+    value.s = temperatureStr;
+    iotElementAdd("temperature", &temperatureElement);
+    iotElementPubAdd(temperatureElement, "", iotValueType_String, value, &temperaturePub);
+    dht22AddTemperatureCallback(&thSensor, temperatureUpdate, NULL);
+#if defined(CONFIG_FAN)
+    humidityFanInit(&fan0, 12, 75);
+    dht22AddHumidityCallback(&thSensor, (DHT22CallBack_t)humidityFanUpdateHumidity, &fan0);
+#endif
     dht22Start(&thSensor);
+#endif
+
+#if defined( CONFIG_LIGHTS_1) || defined(CONFIG_LIGHTS_2) || defined(CONFIG_LIGHTS_3) || defined(CONFIG_MOTION)
+    switchStart();
+#endif
+    iotStart();
 }
