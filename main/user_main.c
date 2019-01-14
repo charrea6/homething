41ae7ab21f382caf4ae181a9d763c58572bbe71c
Adam Charrett <adcharre@cisco.com>
2019-01-14 20:37:23 +0000

    Add details on pin allocations for the different profiles. Use the same pinouts as for the currently install lua firmware.

--- main/user_main.c
+++ main/user_main.c
@@ -1,6 +1,7 @@
 #include <stddef.h>
 #include <stdio.h>
 #include <string.h>
+#include <time.h>
 
 #include "esp_system.h"
 #include "esp_wifi.h"
@@ -23,16 +24,50 @@
 #include "humidityfan.h"
 #include "updater.h"
 
+/* 
+  Pin Allocations
+  ---------------
+
+  | Profile  | Functions
+  |----------|----------------------------------------------------
+  | Bathroom | Light/Switch/Motion/Temperature/Humidity Fan
+  | Utility  | 3x Light/3x Switch/Motion/Temperature/Humidity Fan
+  | Light    | Light/Switch
+  | Doorbell | doorbell(Switch)
+  
+  |    Pins       |             Profile                   |
+  |GPIO | NodeMcu | Bathroom | Doorbell | Light | Utility |
+  |-----|---------|----------|----------|-------|---------|
+  |  0  |   D3    | Light    |          | Light | Light 1 |
+  |  1  |   TX    | TX       | TX       | TX    | TX      |
+  |  2  |   D4    |          |          |       | Light 2 |
+  |  3  |   RX    | RX       | RX       | RX    | RX      |
+  |  4  |   D2    | DHT22    |          |       | DHT22   |
+  |  5  |   D1    |          |          |       | Switch 2|
+  |6-11 |   --    |  --      |  --      | --    |  --     |  
+  | 12  |   D6    | Switch   | Switch   | Switch| Switch 1|
+  | 13  |   D7    | Motion   |          |       | Motion  |
+  | 14  |   D5    | Fan      |          |       | Fan     |
+  | 15  |   D8    |          |          |       | Switch 3|
+  | 16  |   D0    |          |          |       | Light 3 |
+
+GPIO0(D3) - used to indicate to bootloader to go into upload mode + tied to Flash button.
+GPIO16(D0) - possibly tied to RST to allow exit from deep sleep by pulling GPIO16 low.
+*/
+
 //
 // Lights
 // 
 #if defined( CONFIG_LIGHTS_1) || defined(CONFIG_LIGHTS_2) || defined(CONFIG_LIGHTS_3)
+#define ENABLE_LIGHT_1
 static Light_t light0;
 #endif
 #if defined(CONFIG_LIGHTS_2) || defined(CONFIG_LIGHTS_3)
+#define ENABLE_LIGHT_2
 static Light_t light1;
 #endif
 #if defined(CONFIG_LIGHTS_3)
+#define ENABLE_LIGHT_3
 static Light_t light2;
 #endif
 
@@ -45,7 +80,9 @@ static char temperatureStr[6];
 static iotElement_t *temperatureElement;
 static iotElementPub_t *temperaturePub;
 
+#if defined(CONFIG_FAN)
 static HumidityFan_t fan0;
+#endif
 #endif
 
 #if defined(CONFIG_MOTION)
@@ -78,21 +115,25 @@ static void setupLight(Light_t *light, int switchPin, int relayPin)
 
 void app_main(void)
 {
+    struct timeval tv = {.tv_sec = 0, .tv_usec=0};
     ESP_ERROR_CHECK( nvs_flash_init() );
+
+    settimeofday(&tv, NULL);
     
     iotInit(CONFIG_ROOM);
-#if defined( CONFIG_LIGHTS_1) || defined(CONFIG_LIGHTS_2) || defined(CONFIG_LIGHTS_3)
-    setupLight(&light0, 14, 5);
+    
+#ifdef ENABLE_LIGHT_1
+    setupLight(&light0, 12, 3);
 #endif
-#if defined(CONFIG_LIGHTS_2) || defined(CONFIG_LIGHTS_3)
-    setupLight(&light1, 12, 4);
+#ifdef ENABLE_LIGHT_2
+    setupLight(&light1, 2, 5);
 #endif
-#if defined(CONFIG_LIGHTS_3)
-    setupLight(&light2, 13, 0);
+#ifdef ENABLE_LIGHT_3
+    setupLight(&light2, 15, 16);
 #endif
 
 #if defined(CONFIG_MOTION)    
-    motionInit(&motion0, 16);
+    motionInit(&motion0, 13);
 #endif
 
 #if defined(CONFIG_DHT22) 
@@ -105,7 +146,7 @@ void app_main(void)
     iotElementPubAdd(temperatureElement, "", iotValueType_String, false, value, &temperaturePub);
     dht22AddTemperatureCallback(&thSensor, temperatureUpdate, NULL);
 #if defined(CONFIG_FAN)
-    humidityFanInit(&fan0, 12, 75);
+    humidityFanInit(&fan0, 14, 75);
     dht22AddHumidityCallback(&thSensor, (DHT22CallBack_t)humidityFanUpdateHumidity, &fan0);
 #endif
     dht22Start(&thSensor);
