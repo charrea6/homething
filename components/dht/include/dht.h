35f775895511aab64ca04c807c0813544a667268
Adam Charrett <adcharre@synamedia.com>
2019-11-24 16:33:30 +0000

    Add ability to control 2 Fans via 2 different DHT sensors.

--- components/dht/include/dht.h
+++ components/dht/include/dht.h
@@ -12,16 +12,17 @@ typedef struct DHT22CBEntry {
     void *userData;
 } DHT22CBEntry_t;
 
-typedef struct {
+typedef struct DHT22Sensor {
     int8_t pin;
     int8_t nrofTemperatureCBs;
     int8_t nrofHumidityCBs;
     DHT22CBEntry_t temperatureCBs[DHT22_MAX_CALLBACKS];
     DHT22CBEntry_t humidityCBs[DHT22_MAX_CALLBACKS];
+    struct DHT22Sensor *next;
 }DHT22Sensor_t;
 
 void dht22Init(DHT22Sensor_t *sensor, int8_t pin);
 void dht22AddHumidityCallback(DHT22Sensor_t *sensor, DHT22CallBack_t cb, void *userData);
 void dht22AddTemperatureCallback(DHT22Sensor_t *sensor, DHT22CallBack_t cb, void *userData);
-void dht22Start(DHT22Sensor_t *sensor);
+void dht22Start(void);
 #endif
