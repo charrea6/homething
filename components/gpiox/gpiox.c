4852bd4b62ca1f52ecadc317b6acacd23892e2ea
charrea6 <adam@dvbstreamer.org>
2019-12-08 20:54:14 +0000

    Add initial implementation of GPIOX to support internal and expander GPIO pins

---
+++ components/gpiox/gpiox.c
@@ -0,0 +1,169 @@
+#include <stdint.h>
+#include "esp_log.h"
+#include "esp8266/gpio_struct.h"
+#include "driver/gpio.h"
+#include "pcf8574.h"
+#include "gpiox.h"
+
+static const char *TAG="GPIOX";
+
+#if CONFIG_GPIOX_NROF_EXPANDERS > 0
+static uint8_t expander_pin_settings[CONFIG_GPIOX_NROF_EXPANDERS] = {0};
+static i2c_dev_t expander_devices[CONFIG_GPIOX_NROF_EXPANDERS];
+#endif
+
+#define BASE_ADDR 0x40
+
+int gpiox_init(void)
+{
+#if CONFIG_GPIOX_NROF_EXPANDERS > 0
+    for (int i=0; i < CONFIG_GPIOX_NROF_EXPANDERS; i++)
+    {
+        if (pcf8574_init_desc(&expander_devices[i], 0, BASE_ADDR + (i * 2), CONFIG_GPIOX_SDA_PIN, CONFIG_GPIOX_SCL_PIN) != ESP_OK)
+        {
+            ESP_LOGE(TAG, "Failed to create PCF8574 device for expander %d", i);
+            return 1;
+        }
+    }
+#endif
+    return 0;
+}
+
+int gpiox_setup(GPIOX_Pins_t *pins, GPIOX_Mode_t mode)
+{
+    if (pins->pins[0] != 0)
+    {
+        for (int i=0; i < GPIO_NUM_MAX; i++)
+        {
+            if (GPIOX_PINS_IS_SET(*pins, i))
+            {
+                gpio_config_t config;
+
+                config.pin_bit_mask = 1<<i;
+                config.intr_type = GPIO_INTR_DISABLE;
+                switch(mode)
+                {
+                    case GPIOX_MODE_OUT: 
+                    config.mode = GPIO_MODE_OUTPUT;
+                    break;
+                    case GPIOX_MODE_IN_FLOAT:
+                    config.mode = GPIO_MODE_INPUT;
+                    config.pull_down_en = GPIO_PULLDOWN_DISABLE;
+                    config.pull_up_en = GPIO_PULLUP_DISABLE;
+                    break;
+                    case GPIOX_MODE_IN_PULLUP:
+                    config.mode = GPIO_MODE_INPUT;
+                    config.pull_down_en = GPIO_PULLDOWN_DISABLE;
+                    config.pull_up_en = GPIO_PULLUP_ENABLE;
+                    break;
+                    case GPIOX_MODE_IN_PULLDOWN:
+                    config.mode = GPIO_MODE_INPUT;
+                    config.pull_down_en = GPIO_PULLDOWN_ENABLE;
+                    config.pull_up_en = GPIO_PULLUP_DISABLE;
+                    break;
+                    default:
+                    return 1;
+                }
+                if (gpio_config(&config) != ESP_OK)
+                {
+                    ESP_LOGE(TAG, "Invalid config for pin %d", i);
+                    return 1;
+                }
+            }
+        }
+    }
+#if CONFIG_GPIOX_NROF_EXPANDERS > 0
+    if (pins->pins[1] != 0)
+    {
+        for (int i = 0; i < CONFIG_GPIOX_NROF_EXPANDERS; i ++)
+        {
+            uint8_t expander_pins = pins->pins[1] >> (8 * i);
+            switch(mode)
+            {
+                case GPIOX_MODE_OUT: 
+                expander_pin_settings[i] &= ~expander_pins;
+                break;
+                case GPIOX_MODE_IN_PULLUP:
+                expander_pin_settings[i] |= expander_pins;
+                break;
+                default:
+                return 1;
+            }
+            if (pcf8574_port_write(expander_devices[i], expander_pin_settings[i]) != ESP_OK)
+            {
+                ESP_LOGE(TAG, "Pin setup failed for expander %d", i);
+                return 1;
+            }
+        }
+    }
+#endif
+    return 0;   
+}
+
+int gpiox_get_pins(GPIOX_Pins_t *pins, GPIOX_Pins_t *values)
+{
+    GPIOX_PINS_CLEAR_ALL(*values);
+    if (pins->pins[0] != 0)
+    {
+        uint32_t internal_pins = pins->pins[0] & 0xffff;
+        if (internal_pins != 0)
+        {
+            values->pins[0] = GPIO.in & internal_pins;
+        }
+        if (GPIOX_PINS_IS_SET(*pins, 16))
+        {
+            if (gpio_get_level(16) == 1)
+            {
+                GPIOX_PINS_SET(*values, 16);
+            }
+        }
+    }
+#if CONFIG_GPIOX_NROF_EXPANDERS > 0
+    if (pins->pins[1] != 0)
+    {
+        for (int i = 0; i < CONFIG_GPIOX_NROF_EXPANDERS; i ++)
+        {
+            uint8_t expander_pins = pins->pins[1] >> (8 * i);
+            uint8_t value;
+            if (pcf8574_port_read(expander_devices[i], &value) != ESP_OK)
+            {
+                ESP_LOGE(TAG, "Pin read failed for expander %d", i);
+                return 1;
+            }
+            values->pins[1] |= (value & expander_pins) << (i * 8);
+        }
+    }
+#endif
+    return 0;
+}
+
+int gpiox_set_pins(GPIOX_Pins_t *pins, GPIOX_Pins_t *values)
+{
+    if (pins->pins[0] != 0)
+    {
+        for (int i=0; i < GPIO_NUM_MAX; i++)
+        {
+            if (GPIOX_PINS_IS_SET(*pins, i))
+            {
+                gpio_set_level(i, GPIOX_PINS_IS_SET(*values, i));
+            }
+        }
+    }
+#if CONFIG_GPIOX_NROF_EXPANDERS > 0
+    if (pins->pins[1] != 0)
+    {
+        for (int i = 0; i < CONFIG_GPIOX_NROF_EXPANDERS; i ++)
+        {
+            uint8_t expander_pins = pins->pins[1] >> (8 * i);
+            uint8_t value = ((values->pins[1] >> (8 * 1)) & expander_pins) | (expander_pin_settings[i] & ~expander_pins);
+            if (pcf8574_port_write(expander_devices[i], value) != ESP_OK)
+            {
+                ESP_LOGE(TAG, "Pin write failed for expander %d", i);
+                return 1;
+            }
+            expander_pin_settings[i] = value;
+        }
+    }
+#endif
+    return 0;
+}
---
+++ components/gpiox/include/gpiox.h
@@ -0,0 +1,90 @@
+#ifndef _GPIOX_H_
+#define _GPIOX_H_
+#include <stdint.h>
+#include "sdkconfig.h"
+
+// Nodemcu Pin Names
+#define D0 16
+#define D1 5
+#define D2 4
+#define D3 0
+#define D4 2
+#define D5 14
+#define D6 12
+#define D7 13
+#define D8 15
+#define RX 3
+#define TX 1
+#define SD3 10
+#define SD2 9
+
+#if CONFIG_GPIOX_NROF_EXPANDERS > 0
+#define GPIOX_PINS_SIZE 2
+#define GPIOX_BASE 24
+#define X1 (GPIOX_BASE)
+#define X2 (GPIOX_BASE + 1)
+#define X3 (GPIOX_BASE + 2)
+#define X4 (GPIOX_BASE + 3)
+#define X5 (GPIOX_BASE + 4)
+#define X6 (GPIOX_BASE + 5)
+#define X7 (GPIOX_BASE + 6)
+#define X8 (GPIOX_BASE + 7)
+
+#if CONFIG_GPIOX_NROF_EXPANDERS > 1
+#define X9 (GPIOX_BASE + 8)
+#define X10 (GPIOX_BASE + 9)
+#define X11 (GPIOX_BASE + 10)
+#define X12 (GPIOX_BASE + 11)
+#define X13 (GPIOX_BASE + 12)
+#define X14 (GPIOX_BASE + 13)
+#define X15 (GPIOX_BASE + 14)
+#define X16 (GPIOX_BASE + 15)
+#endif
+
+#if CONFIG_GPIOX_NROF_EXPANDERS > 2
+#define X17 (GPIOX_BASE + 16)
+#define X18 (GPIOX_BASE + 17)
+#define X19 (GPIOX_BASE + 18)
+#define X20 (GPIOX_BASE + 19)
+#define X21 (GPIOX_BASE + 20)
+#define X22 (GPIOX_BASE + 21)
+#define X23 (GPIOX_BASE + 22)
+#define X24 (GPIOX_BASE + 23)
+#endif
+
+#if CONFIG_GPIOX_NROF_EXPANDERS > 3
+#define X25 (GPIOX_BASE + 24)
+#define X26 (GPIOX_BASE + 25)
+#define X27 (GPIOX_BASE + 26)
+#define X28 (GPIOX_BASE + 27)
+#define X29 (GPIOX_BASE + 28)
+#define X30 (GPIOX_BASE + 29)
+#define X31 (GPIOX_BASE + 30)
+#define X32 (GPIOX_BASE + 31)
+#endif
+
+#else
+#define GPIOX_PINS_SIZE 1
+#endif
+
+typedef struct {
+    uint32_t pins[GPIOX_PINS_SIZE];
+} GPIOX_Pins_t;
+
+typedef enum {
+    GPIOX_MODE_OUT,
+    GPIOX_MODE_IN_FLOAT,
+    GPIOX_MODE_IN_PULLUP,
+    GPIOX_MODE_IN_PULLDOWN,
+}GPIOX_Mode_t;
+
+#define GPIOX_PINS_SET(_pins, pin) ((_pins).pins[pin/32] = 1 << (pin % 32))
+#define GPIOX_PINS_CLEAR(_pins, pin) ((_pins).pins[pin/32] = ((pins).pins[pin/32] & ~(1 << (pin % 32))))
+#define GPIOX_PINS_CLEAR_ALL(_pins) do{int i; for (i = 0; i < GPIOX_PINS_SIZE;i++) (_pins).pins[i] = 0; }while(0)
+#define GPIOX_PINS_IS_SET(_pins, pin) (((_pins).pins[pin/32] & 1 << (pin % 32)) != 0)
+
+int gpiox_init(void);
+int gpiox_setup(GPIOX_Pins_t *pins, GPIOX_Mode_t mode);
+int gpiox_get_pins(GPIOX_Pins_t *pins, GPIOX_Pins_t *values);
+int gpiox_set_pins(GPIOX_Pins_t *pins, GPIOX_Pins_t *values);
+#endif
