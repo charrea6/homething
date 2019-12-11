7953d4dbb1079b4d992dfd8bbea31572562e02b1
charrea6 <adam@dvbstreamer.org>
2019-12-11 22:44:26 +0000

    Get output pins working on expander board.

--- components/relay/relay.c
+++ components/relay/relay.c
@@ -1,5 +1,5 @@
 #include <stdbool.h>
-#include "driver/gpio.h"
+#include "gpiox.h"
 #include "relay.h"
 #include "sdkconfig.h"
 #include "esp_log.h"
@@ -8,27 +8,31 @@ static const char TAG[] = "relay";
 
 void relayInit(int8_t pin, Relay_t *relay)
 {
-    gpio_config_t config;
+    GPIOX_Pins_t pins;
+    GPIOX_PINS_CLEAR_ALL(pins);
+    GPIOX_PINS_SET(pins, pin);
 
-    relay->pin = pin;    
-    config.pin_bit_mask = 1 << pin;
-    config.mode = GPIO_MODE_DEF_OUTPUT;
-    config.pull_down_en = GPIO_PULLDOWN_DISABLE;
-    config.pull_up_en = GPIO_PULLUP_DISABLE;
-    config.intr_type = GPIO_INTR_DISABLE;
-    gpio_config(&config);
+    gpioxSetup(&pins, GPIOX_MODE_OUT);
+    relay->pin = pin;
     relaySetState(relay, RelayState_Off);
 }
 
 void relaySetState(Relay_t *relay, RelayState_t state)
 {
     int l;
+    GPIOX_Pins_t pins, values;
+    GPIOX_PINS_CLEAR_ALL(pins);
+    GPIOX_PINS_CLEAR_ALL(values);
+    GPIOX_PINS_SET(pins, relay->pin);
 #ifdef CONFIG_RELAY_ON_HIGH
     l = state == RelayState_On ? 1:0;
 #else
     l = state == RelayState_On ? 0:1;
 #endif
+    if (l){
+        GPIOX_PINS_SET(values, relay->pin);
+    }
     ESP_LOGI(TAG, "Relay %d: Set state %d (pin value %d)", relay->pin, state, l);
-    gpio_set_level(relay->pin, l);
+    gpioxSetPins(&pins, &values);
     relay->state = state;
 }
