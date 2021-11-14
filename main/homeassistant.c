bd4b82cfcf66f68f39a55e9dfb8462efbd69a207
charrea6 <adam@dvbstreamer.org>
2021-11-14 13:51:55 +0000

    Add Relay discovery to homeassistant

--- main/homeassistant.c
+++ main/homeassistant.c
@@ -12,15 +12,28 @@
 static const char *TAG="hass";
 static const char *DESC="desc";
 
-static const char *DEGREES_C = "\xC2\xB0""C";
+static const char *DEGREES_C="\xC2\xB0""C";
+
+#if CONFIG_IDF_TARGET_ESP32
+    static const char *MODEL="esp32";
+#else
+    static const char *MODEL="esp8266";
+#endif
+
+struct DeviceDetails {
+    char identifier[13];
+    char *deviceDescription;
+};
 
 static void onMqttStatusUpdated(void *user,  NotificationsMessage_t *message);
 static void sendDiscoveryMessages();
 static char* deviceGetDescription(void);
-static void processElement(char *identifier, char *deviceDescription, iotElement_t element);
-static void processSensor(char *identifier, char *deviceDescription, iotElement_t element);
-static void processSwitch(char *identifier, char *deviceDescription, iotElement_t element);
-static void addDeviceDetails(char *identifier, char *deviceDescription, cJSON * obj);
+static void processElement(struct DeviceDetails *deviceDetails, iotElement_t element);
+static void processSensor(struct DeviceDetails *deviceDetails, iotElement_t element);
+static void processSwitch(struct DeviceDetails *deviceDetails, iotElement_t element);
+static void addDeviceDetails(struct DeviceDetails *deviceDetails, cJSON * obj);
+static void sendDiscoveryMessage(struct DeviceDetails *deviceDetails, const char* type,
+                                 iotElement_t element, const char *pubName, cJSON *object);
 
 void homeAssistantDiscoveryInit(void)
 {
@@ -38,37 +51,37 @@ static void sendDiscoveryMessages()
 {
     iotElementIterator_t iterator = IOT_ELEMENT_ITERATOR_START;
     iotElement_t element;
-    char identifier[13];
+    struct DeviceDetails details;
     uint8_t mac[6];
     esp_read_mac(mac, ESP_MAC_WIFI_STA);
-    sprintf(identifier, "%02x%02x%02x%02x%02x%02x", mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]);
-    char *deviceDescription = deviceGetDescription();
+    sprintf(details.identifier, "%02x%02x%02x%02x%02x%02x", mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]);
+    details.deviceDescription = deviceGetDescription();
 
     while(iotElementIterate(&iterator, true, &element)) {
-        processElement(identifier, deviceDescription, element);
+        processElement(&details, element);
     }
-    if (deviceDescription != NULL) {
-        free(deviceDescription);
+    if (details.deviceDescription != NULL) {
+        free(details.deviceDescription);
     }
 }
 
-static void processElement(char *identifier, char *deviceDescription, iotElement_t element)
+static void processElement(struct DeviceDetails *deviceDetails, iotElement_t element)
 {
     const iotElementDescription_t *description = iotElementGetDescription(element);
     switch(description->type) {
     case IOT_ELEMENT_TYPE_SENSOR_HUMIDITY:
     case IOT_ELEMENT_TYPE_SENSOR_TEMPERATURE:
-        processSensor(identifier, deviceDescription, element);
+        processSensor(deviceDetails, element);
         break;
     case IOT_ELEMENT_TYPE_SWITCH:
-        processSwitch(identifier, deviceDescription, element);
+        processSwitch(deviceDetails, element);
         break;
     default:
         break;
     }
 }
 
-static void processSensor(char *identifier, char *deviceDescription, iotElement_t element)
+static void processSensor(struct DeviceDetails *deviceDetails, iotElement_t element)
 {
     const iotElementDescription_t *description = iotElementGetDescription(element);
     int pubId;
@@ -102,29 +115,8 @@ static void processSensor(char *identifier, char *deviceDescription, iotElement_
         cJSON_AddStringToObject(object, "device_class", deviceClass);
         cJSON_AddStringToObject(object, "unit_of_measurement", unitOfMeasurement);
 
-        const char *pubName = iotElementGetPubName(element, pubId);
-        const char *elementName = iotElementGetName(element);
-        char *uniqueId = NULL;
-
-        asprintf(&uniqueId, "%s:%s:%s", identifier, elementName, pubName);
-        if (uniqueId != NULL) {
-            cJSON_AddStringToObject(object, "unique_id", uniqueId);
-        }
-
-        char *name = NULL;
-        if (pubName[0] == 0) {
-            asprintf(&name, "%s %s", deviceDescription, elementName);
-        } else {
-            asprintf(&name, "%s %s %s", deviceDescription, elementName, pubName);
-        }
-        if (name != NULL) {
-            cJSON_AddStringToObject(object, "name", name);
-            free(name);
-        }
-
         char *stateTopic = NULL;
         size_t stateTopicSize = 0;
-
         iotElementGetPubPath(element, pubId, stateTopic, &stateTopicSize);
         if (stateTopicSize == 0) {
             cJSON_Delete(object);
@@ -140,49 +132,127 @@ static void processSensor(char *identifier, char *deviceDescription, iotElement_
             cJSON_Delete(object);
             continue;
         }
+
         cJSON_AddStringToObject(object, "state_topic", stateTopic);
         free(stateTopic);
 
-        addDeviceDetails(identifier, deviceDescription, object);
-
-        char *json = cJSON_PrintUnformatted(object);
-        cJSON_Delete(object);
-        if (json == NULL) {
-            continue;
-        }
-
-        char *discoveryPath = NULL;
-        asprintf(&discoveryPath, "homeassistant/sensor/%s/%s_%s/config", identifier,
-                 iotElementGetName(element), iotElementGetPubName(element, pubId));
-        printf("Element = %p Name = %s path = %s\n", element, iotElementGetName(element), discoveryPath);
-
-        if (discoveryPath != NULL) {
-            iotMqttPublish(discoveryPath, json, strlen(json), 0, 1);
-            free(discoveryPath);
-        }
-        free(json);
+        const char *pubName = iotElementGetPubName(element, pubId);
+        sendDiscoveryMessage(deviceDetails, "sensor", element, pubName, object);
     }
 }
 
-static void processSwitch(char *identifier, char *deviceDescription, iotElement_t element)
+static void processSwitch(struct DeviceDetails *deviceDetails, iotElement_t element)
 {
+    const iotElementDescription_t *description = iotElementGetDescription(element);
+    const char *pubName = NULL;
+    const char *ctrlName = NULL;
+    if ((description->nrofPubs == 0) || (description->nrofSubs == 0)) {
+        return;
+    }
+    pubName = iotElementGetPubName(element, 0);
+    ctrlName = iotElementGetSubName(element, 0);
+    cJSON *object = cJSON_CreateObject();
+    if (object == NULL) {
+        return;
+    }
+    char *topic = NULL;
+    asprintf(&topic, "~/%s", pubName);
+    if (topic == NULL) {
+        cJSON_Delete(object);
+        return;
+    }
+    cJSON_AddStringToObject(object, "state_topic", topic);
+    free(topic);
 
+    asprintf(&topic, "~/%s", ctrlName);
+    if (topic == NULL) {
+        cJSON_Delete(object);
+        return;
+    }
+    cJSON_AddStringToObject(object, "command_topic", topic);
+    free(topic);
+
+    size_t topicSize = 0;
+    iotElementGetBasePath(element, NULL, &topicSize);
+    if (topicSize == 0) {
+        cJSON_Delete(object);
+        return;
+    }
+    topic = malloc(topicSize);
+    if (topic == NULL) {
+        cJSON_Delete(object);
+        return;
+    }
+    if (iotElementGetBasePath(element, topic, &topicSize) == NULL) {
+        free(topic);
+        cJSON_Delete(object);
+        return;
+    }
+    cJSON_AddStringToObject(object, "~", topic);
+    free(topic);
+
+    cJSON_AddStringToObject(object, "payload_off", "off");
+    cJSON_AddStringToObject(object, "payload_on", "on");
+
+    sendDiscoveryMessage(deviceDetails, "switch", element, NULL, object);
 }
 
-static void addDeviceDetails(char *identifier, char *deviceDescription, cJSON * obj)
+static void sendDiscoveryMessage(struct DeviceDetails *deviceDetails, const char* type,
+                                 iotElement_t element, const char *pubName, cJSON *object)
+{
+    const char *elementName = iotElementGetName(element);
+    char *uniqueId = NULL;
+
+    asprintf(&uniqueId, "%s:%s:%s", deviceDetails->identifier, elementName, pubName == NULL ? "":pubName);    
+    if (uniqueId != NULL) {
+        cJSON_AddStringToObject(object, "unique_id", uniqueId);
+    }
+
+    char *name = NULL;
+    if ((pubName == NULL) || (pubName[0] == 0)) {
+        asprintf(&name, "%s %s", deviceDetails->deviceDescription, elementName);
+    } else {
+        asprintf(&name, "%s %s %s", deviceDetails->deviceDescription, elementName, pubName);
+    }
+    if (name != NULL) {
+        cJSON_AddStringToObject(object, "name", name);
+        free(name);
+    }
+
+    addDeviceDetails(deviceDetails, object);
+
+    char *json = cJSON_PrintUnformatted(object);
+    cJSON_Delete(object);
+    if (json == NULL) {
+        return;
+    }
+
+    char *discoveryPath = NULL;
+    asprintf(&discoveryPath, "homeassistant/%s/%s/%s_%s/config", type, deviceDetails->identifier,
+            iotElementGetName(element), pubName == NULL ? "":pubName);
+    if (discoveryPath != NULL) {
+        iotMqttPublish(discoveryPath, json, strlen(json), 0, 1);
+        free(discoveryPath);
+    }
+    free(json);
+}
+
+static void addDeviceDetails(struct DeviceDetails *deviceDetails, cJSON * obj)
 {
     cJSON *devObject = cJSON_AddObjectToObject(obj, "dev");
     if (devObject == NULL) {
         return;
     }
 
-    cJSON_AddStringToObject(devObject, "ids", identifier);
+    cJSON_AddStringToObject(devObject, "ids", deviceDetails->identifier);
 
-    if (deviceDescription) {
-        cJSON_AddStringToObject(devObject, "name", deviceDescription);
+    if (deviceDetails->deviceDescription) {
+        cJSON_AddStringToObject(devObject, "name", deviceDetails->deviceDescription);
     }
 
     cJSON_AddStringToObject(devObject, "sw", updaterGetVersion());
+    cJSON_AddStringToObject(devObject, "mf", "Homething");
+    cJSON_AddStringToObject(devObject, "mdl", MODEL);
 }
 
 static char* deviceGetDescription(void)
