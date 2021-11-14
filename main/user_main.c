bd061e3e784bde6b964f9eea2eca641b88b33055
charrea6 <adam@dvbstreamer.org>
2021-11-14 15:19:11 +0000

    Make home assistant support configurable

--- main/user_main.c
+++ main/user_main.c
@@ -42,7 +42,6 @@ void app_main(void)
 #if defined(CONFIG_BME280) || defined(CONFIG_SI7021) || defined(CONFIG_GPIOX_EXPANDERS)
     ESP_ERROR_CHECK( i2cdev_init() );
 #endif
-    cJSON_InitHooks(NULL);
 
     notificationsInit();
 
@@ -62,7 +61,10 @@ void app_main(void)
 
     updaterInit();
 
+#ifdef CONFIG_HOMEASSISTANT
+    cJSON_InitHooks(NULL);
     homeAssistantDiscoveryInit();
+#endif
 
     switchStart();
     iotStart();
